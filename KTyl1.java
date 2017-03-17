a9faf320d2a55de956bbbf55e3f8d34b2b108718
kkriiska <kkriiska@users.noreply.github.com>
2017-03-17 13:38:15 +0200

    Delete KTyl1.java

--- KTyl1.java
+++
@@ -1,9 +0,0 @@
-public class KT {
-	public static void main(String[] args) {
-		int x=20;
-		int y=2;
-		int k = x * y;
-	
-	System.out.println("Ruutjuur kahe arvu parameetrist on: " +Math.sqrt(k));
-	}
-}
