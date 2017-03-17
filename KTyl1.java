290f47c2e3310b07994ee537a016ca77b0b71b79
kkriiska <kkriiska@users.noreply.github.com>
2017-03-17 12:59:21 +0200

    Add files via upload

---
+++ KTyl1.java
@@ -0,0 +1,9 @@
+public class KT {
+	public static void main(String[] args) {
+		int x=20;
+		int y=2;
+		int k = x * y;
+	
+	System.out.println("Ruutjuur kahe arvu parameetrist on: " +Math.sqrt(k));
+	}
+}
